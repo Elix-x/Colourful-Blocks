791748004f8eba8b51321e5d71775f028c256dad
elix-x <agent-lego@mail.ru>
2016-05-05 20:25:30 +0200

    Removed unused classes
    Removed unused classes.

--- 1.7.10/src/main/java/code/elix_x/coremods/colourfulblocks/exceptions/MalformedColorIdentifierException.java
+++
@@ -1,9 +0,0 @@
-package code.elix_x.coremods.colourfulblocks.exceptions;
-
-public class MalformedColorIdentifierException extends Exception {
-
-	public MalformedColorIdentifierException(String cause) {
-		super(cause);
-	}
-	
-}
--- 1.7.10/src/main/java/code/elix_x/coremods/colourfulblocks/exceptions/MalformedItemStackIdentifierException.java
+++
@@ -1,9 +0,0 @@
-package code.elix_x.coremods.colourfulblocks.exceptions;
-
-public class MalformedItemStackIdentifierException extends Exception {
-
-	public MalformedItemStackIdentifierException(String cause) {
-		super(cause);
-	}
-	
-}
