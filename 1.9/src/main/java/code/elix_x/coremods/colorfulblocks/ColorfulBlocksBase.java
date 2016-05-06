b60ca7c78137c26a4755067a8aaca50d5e4a64e2
elix-x <agent-lego@mail.ru>
2016-05-06 18:52:33 +0200

    1.3.1 - Bug fixes
    -Fixed compile issues.
    -Fixed mcmod.info.
    -Fixed #12.

--- 1.9/src/main/java/code/elix_x/coremods/colorfulblocks/ColorfulBlocksBase.java
+++ 1.9/src/main/java/code/elix_x/coremods/colorfulblocks/ColorfulBlocksBase.java
@@ -48,7 +48,7 @@ public class ColorfulBlocksBase {
 
 	public static final String MODID = "colorfulblocks";
 	public static final String NAME = "Colorful Blocks";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.3.1";
 
 	@Mod.Instance(MODID)
 	public static ColorfulBlocksBase instance;
